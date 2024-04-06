72ff2a58c8d5f17d31034cb52d5c30cb137704de
nithinvisva <visvanic@gmail.com>
2024-04-06 15:58:01 +0530

    api connection

--- llamaModel/app.py
+++ llamaModel/app.py
@@ -8,6 +8,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 # creating a Flask app 
 app = Flask(__name__) 
+CORS(app, support_credentials=True)
 
 ## Function To get response from LLAma 2 model
 
