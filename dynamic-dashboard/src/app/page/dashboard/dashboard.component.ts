bc01df5a92690f54cc0606d0dce7869cd9b703f3
vishnu-calibraint <vishnuprasath.s@calibraint.com>
2024-04-06 11:25:25 +0530

    FEAT: Intergreted the frontend for dynamic dashboard

--- dynamic-dashboard/src/app/page/dashboard/dashboard.component.ts
+++ dynamic-dashboard/src/app/page/dashboard/dashboard.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { ChartComponent } from '../../shared/chart/chart.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [ChartComponent],
+  imports: [ChartComponent,NgFor],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
+  chartTypes = ['line', 'bar', 'radar'];
+
+  chartData = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+      {
+        label: '# of values',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
 
 }
